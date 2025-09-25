ce18bcc50bbafdf8b098f8004958188a31c3024d
Matyas Murphy <matyas.murphy@icloud.com>
2025-09-25 10:58:47 +0200

    jjj

--- src/main/java/org/example/bankAccounts/StudentBankAccount.java
+++ src/main/java/org/example/bankAccounts/StudentBankAccount.java
@@ -2,8 +2,18 @@ package org.example.bankAccounts;
 
 import org.example.people.BaseBankAccountOwner;
 
+
 public class StudentBankAccount extends BaseBankAccount{
+
+    private String school;
+
     public StudentBankAccount(String uuid, String accountNumber, BaseBankAccountOwner owner, double balance) {
         super(uuid, accountNumber, owner, balance);
     }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) { this.school = school; }
 }
